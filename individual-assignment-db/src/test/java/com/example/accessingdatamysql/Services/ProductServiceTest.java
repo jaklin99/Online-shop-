69b5ce8fb41c0270bf61735d004df691ad8c0b71
jakitoo yanakieva <i428428@fhict.nl>
2021-01-18 10:37:31 +0100

    all done

--- individual-assignment-db/src/test/java/com/example/accessingdatamysql/Services/ProductServiceTest.java
+++ individual-assignment-db/src/test/java/com/example/accessingdatamysql/Services/ProductServiceTest.java
@@ -1,87 +1,87 @@
-//package com.example.accessingdatamysql.Services;
-//
-//import com.example.accessingdatamysql.Repository.ProductRepository;
-//import com.example.accessingdatamysql.modelsTemp.Product;
-//import com.example.accessingdatamysql.modelsTemp.User;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.security.core.parameters.P;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.when;
-//
-//
-//@ExtendWith(MockitoExtension.class)
-//class ProductServiceTest {
-//
-//    @Mock
-//    ProductRepository productRepository;
-//    @InjectMocks
-//    ProductService productService;
-//
-//    @BeforeEach
-//    void setUp() {
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//    }
-//
-//    @Test
-//    void findAll() {
-//        List<Product> productsCreate=new ArrayList<>();
-//        when(productRepository.findAll()).thenReturn(productsCreate);
-//        List<Product> created = productService.findAll();
-//        assertThat(productsCreate).isSameAs(created);
-//    }
-//
-//    @Test
-//    void findByName() {
-//        final String name= "racquet";
-//        final  Product product =new Product();
-//        given(productRepository.findByName(name)).willReturn(Optional.of(product));
-//        final Optional<Product> expected = productService.findByName(name);
-//        assertThat(expected).isNotNull();
-//    }
-//
-//    @Test
-//    void findById() {
-//        final Long id= 1L;
-//        final  Product product =new Product();
-//        given(productRepository.findById(id)).willReturn(Optional.of(product));
-//        final Optional<Product> expected = productService.findById(id);
-//        assertThat(expected).isNotNull();
-//    }
-//
-//    @Test
-//    void existsByName() {
-//    }
-//
-//    @Test
-//    void deleteByName() {
-//    }
-//
-//    @Test
-//    void deleteAll() {
-//    }
-//
-//    @Test
-//    void save() {
-//        Product productCreate=new Product();
-//        productCreate.setProductName("dfgyui");
-//        when(productRepository.save(any(Product.class))).thenReturn(new Product());
-//        Product created = productService.save(productCreate);
-//        assertThat(created.getProductName()).isSameAs(productCreate.getProductName());
-//    }
-//}
+package com.example.accessingdatamysql.Services;
+
+import com.example.accessingdatamysql.Repository.ProductRepository;
+import com.example.accessingdatamysql.modelsTemp.Product;
+import com.example.accessingdatamysql.modelsTemp.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.parameters.P;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    ProductRepository productRepository;
+    @InjectMocks
+    ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAll() {
+        List<Product> productsCreate=new ArrayList<>();
+        when(productRepository.findAll()).thenReturn(productsCreate);
+        List<Product> created = productService.findAll();
+        assertThat(productsCreate).isSameAs(created);
+    }
+
+    @Test
+    void findByName() {
+        final String name= "racquet";
+        final  Product product =new Product();
+        given(productRepository.findByName(name)).willReturn(Optional.of(product));
+        final Optional<Product> expected = productService.findByName(name);
+        assertThat(expected).isNotNull();
+    }
+
+    @Test
+    void findById() {
+        final Long id= 1L;
+        final  Product product =new Product();
+        given(productRepository.findById(id)).willReturn(Optional.of(product));
+        final Optional<Product> expected = productService.findById(id);
+        assertThat(expected).isNotNull();
+    }
+
+    @Test
+    void existsByName() {
+    }
+
+    @Test
+    void deleteByName() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+
+    @Test
+    void save() {
+        Product productCreate=new Product();
+        productCreate.setProductName("dfgyui");
+        when(productRepository.save(any(Product.class))).thenReturn(new Product());
+        Product created = productService.save(productCreate);
+        assertThat(created.getProductName()).isSameAs(productCreate.getProductName());
+    }
+}
--- individual-assignment-db/src/test/java/com/example/accessingdatamysql/Services/UserServiceTest.java
+++ individual-assignment-db/src/test/java/com/example/accessingdatamysql/Services/UserServiceTest.java
@@ -1,97 +1,97 @@
-//package com.example.accessingdatamysql.Services;
+package com.example.accessingdatamysql.Services;
+
+import com.example.accessingdatamysql.Repository.UserRepository;
+import com.example.accessingdatamysql.modelsTemp.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+//@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findByEmail() {
+        final String email= "jakitoo99@gmail.com";
+        final  User user =new User("jak", "jak", "str", "01234567", "jakitoo99@gmail.com", "jakito");
+        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+        final Optional<User> expected = userService.findByEmail(email);
+        assertThat(expected).isNotNull();
+    }
+
+    @Test
+    void existsByEmail() {
 //
-//import com.example.accessingdatamysql.Repository.UserRepository;
-//import com.example.accessingdatamysql.modelsTemp.User;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.when;
-//
-////@RunWith(MockitoJUnitRunner.class)
-//@ExtendWith(MockitoExtension.class)
-//class UserServiceTest {
-//    @Mock
-//    UserRepository userRepository;
-//
-//    @InjectMocks
-//    UserService userService;
-//
-//    @BeforeEach
-//    void setUp() {
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//    }
-//
-//    @Test
-//    void findByEmail() {
 //        final String email= "jakitoo99@gmail.com";
 //        final  User user =new User("jak", "jak", "str", "01234567", "jakitoo99@gmail.com", "jakito");
-//        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+//        when(userRepository.existsByEmail(email)).thenReturn(true);
+//        final Optional<User> expected = userService.findByEmail(email);
+//        assertThat(expected).isNotNull();
+    }
+
+    @Test
+    void existsById() {
+    }
+
+//    @Test
+//    void deleteByEmail() {
+//        final String email= "jakitoo99@gmail.com";
+//        final  User user =new User("jak", "jak", "str", "01234567", "jakitoo99@gmail.com", "jakito");
+//        when(userRepository.deleteByEmail(email)).willReturn(Optional.of(user));
 //        final Optional<User> expected = userService.findByEmail(email);
 //        assertThat(expected).isNotNull();
 //    }
-//
-//    @Test
-//    void existsByEmail() {
-////
-////        final String email= "jakitoo99@gmail.com";
-////        final  User user =new User("jak", "jak", "str", "01234567", "jakitoo99@gmail.com", "jakito");
-////        when(userRepository.existsByEmail(email)).thenReturn(true);
-////        final Optional<User> expected = userService.findByEmail(email);
-////        assertThat(expected).isNotNull();
-//    }
-//
-//    @Test
-//    void existsById() {
-//    }
-//
-////    @Test
-////    void deleteByEmail() {
-////        final String email= "jakitoo99@gmail.com";
-////        final  User user =new User("jak", "jak", "str", "01234567", "jakitoo99@gmail.com", "jakito");
-////        when(userRepository.deleteByEmail(email)).willReturn(Optional.of(user));
-////        final Optional<User> expected = userService.findByEmail(email);
-////        assertThat(expected).isNotNull();
-////    }
-//
-//    @Test
-//    void findAll() {
-//        List<User> usersCreate=new ArrayList<>();
-//        when(userRepository.findAll()).thenReturn(usersCreate);
-//        List<User> created = userService.findAll();
-//        assertThat(usersCreate).isSameAs(created);
-//
-//    }
-//
-//    @Test
-//    void deleteAll() {
-//    }
-//
-//    @Test
-//    void save() {
-//        User userCreate=new User();
-//        userCreate.setName("dfgyui");
-//        when(userRepository.save(any(User.class))).thenReturn(new User());
-//        User created = userService.save(userCreate);
-//        assertThat(created.getName()).isSameAs(userCreate.getName());
-//    }
-//
-//    @Test
-//    void getOne() {
-//    }
-//}
+
+    @Test
+    void findAll() {
+        List<User> usersCreate=new ArrayList<>();
+        when(userRepository.findAll()).thenReturn(usersCreate);
+        List<User> created = userService.findAll();
+        assertThat(usersCreate).isSameAs(created);
+
+    }
+
+    @Test
+    void deleteAll() {
+    }
+
+    @Test
+    void save() {
+        User userCreate=new User();
+        userCreate.setName("dfgyui");
+        when(userRepository.save(any(User.class))).thenReturn(new User());
+        User created = userService.save(userCreate);
+        assertThat(created.getName()).isSameAs(userCreate.getName());
+    }
+
+    @Test
+    void getOne() {
+    }
+}
